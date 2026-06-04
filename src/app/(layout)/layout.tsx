8351661f01274a53b576d393e00cc8445f32f86e
Gerardo Ramirez <gerardoramirez4400@gmail.com>
2026-06-04 16:41:04 -0500

    add google analytics

--- src/app/(layout)/layout.tsx
+++ src/app/(layout)/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import Navbar from "@/components/layout/Navbar";
 import WhatsAppButton from "@/components/ui/WhatsAppButton";
+import Script from "next/script";
 
 const inter = Inter({
   variable: "--font-inter",
@@ -13,6 +14,24 @@ export const metadata: Metadata = {
   title: "Andrés Ricaurte | Estratega en Marketing y Ventas con IA",
   description:
     "Factura más con un sistema de marketing y ventas automatizadas.",
+  metadataBase: new URL("https://andresricaurte.com"),
+  openGraph: {
+    title: "Andrés Ricaurte | Estratega en Marketing y Ventas",
+    description:
+      "Factura más con un sistema de marketing y ventas automatizadas.",
+    url: "https://andresricaurte.com",
+    siteName: "Andrés Ricaurte",
+    images: [
+      {
+        url: "/AndresRicaurteTeams.webp",
+        width: 1822,
+        height: 2256,
+        alt: "Andrés Ricaurte Portada",
+      },
+    ],
+    locale: "es_CO",
+    type: "website",
+  },
 };
 
 export default function RootLayout({
@@ -22,6 +41,18 @@ export default function RootLayout({
 }>) {
   return (
     <>
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=G-EDXD4WBTZP`}
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-EDXD4WBTZP');
+        `}
+      </Script>
       <Navbar />
       <main>{children}</main>
       <WhatsAppButton />
